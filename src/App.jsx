7bd2d3c2828ca7ef0fa357c4a770aa93d8040637
aishayus <aishayussuff2005@gmail.com>
2024-08-29 19:25:17 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Add from './pages/Add';
@@ -10,18 +10,18 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function App() {
   const url = 'https://final-project-bd-1.onrender.com/'
   return (
-    <BrowserRouter>
-    <ToastContainer />
-      <Navbar />
-      <hr />
-      <div className='flex max-padd-container'>
-        <Sidebar />
-        <Routes>
-          <Route path='/add' element={<Add url={url} />}/>
-          <Route path='/list' element={<List url={url} />}/>
-          <Route path='/orders' element={<Orders url={url} />}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      <ToastContainer />
+        <Navbar />
+        <hr />
+        <div className='flex max-padd-container'>
+          <Sidebar />
+          <Routes>
+            <Route path='/add' element={<Add url={url} />}/>
+            <Route path='/list' element={<List url={url} />}/>
+            <Route path='/orders' element={<Orders url={url} />}/>
+          </Routes>
+        </div>
+    </>
   )
 }
